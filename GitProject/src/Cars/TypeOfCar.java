ce59db4c9579305e7cf04b1dfe8efe9ab8226e02
Dhiraj <dhirajbhosale7777@gmail.com>
2022-11-15 23:11:54 +0530

    my first command

---
+++ GitProject/src/Cars/TypeOfCar.java
@@ -0,0 +1,14 @@
+package Cars;
+
+public class TypeOfCar {
+	public static void main(String[] args) {
+		String namesofcar = "swift";
+		if(namesofcar=="swift") {
+			
+			System.out.println("type of car is suzuki");
+			
+		}else {
+			System.out.println("type of car is nexa");
+		}
+	}
+}
