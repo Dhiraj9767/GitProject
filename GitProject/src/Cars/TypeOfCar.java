46d3760197aa8db06d6d1b77a5c3173363faa3a5
Dhiraj9767 <dhirajbhosale7777@gmail.com>
2022-11-16 08:48:54 +0530

    second commit

--- GitProject/src/Cars/TypeOfCar.java
+++ GitProject/src/Cars/TypeOfCar.java
@@ -1,9 +1,15 @@
 package Cars;
 
+import java.util.Scanner;
+
 public class TypeOfCar {
 	public static void main(String[] args) {
-		String namesofcar = "swift";
-		if(namesofcar=="swift") {
+		System.out.println("program started ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Car Name");
+		String namesofcar = sc.next();
+		
+		if(namesofcar=="ertiga") {
 			
 			System.out.println("type of car is suzuki");
 			
@@ -11,6 +17,6 @@ public class TypeOfCar {
 			System.out.println("type of car is nexa");
 		}
 		
-		System.out.println("close the programm");
+		System.out.println("end the program");
 	}
 }
